46311e1232f454c143a2744399389474699e6684
Stitch <stitch78140@gmail.com>
2025-09-20 11:55:09 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function App(){
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Civils Déplacés v6</h1>
+      <p>Ici viendra l'interface de calcul (DMJ, Amin/Bmin, HS, HSM, etc.)</p>
+      <p className="mt-4 text-red-600">Crédit de 1 RCJ au titre du DP sur le R (samedi)</p>
+      <p className="mt-2 text-red-600">Crédit de 1,5 ou 2 RCJ + 1 RL au titre du DP sur le RH (dimanche)</p>
+    </div>
+  )
+}
